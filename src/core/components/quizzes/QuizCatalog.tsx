f186d79ad2e603f75b0cd647b4918156ea26e3a2
MichiMensing <mensing.michi@gmail.com>
2025-01-11 23:31:20 -0500

    feat: add quiz button

--- src/core/components/quizzes/QuizCatalog.tsx
+++ src/core/components/quizzes/QuizCatalog.tsx
@@ -1,44 +1,43 @@
-import React, { FunctionComponent} from "react";
+import React, { FunctionComponent, useId, useState } from "react";
 import "./QuizCatalog.css";
 import Quiz from "./Quiz";
 import { t } from "i18next";
-import QuizSettings from "./QuizSettings";
+import { v4 as uuidv4 } from 'uuid';
 
-const quizzes = [(
-  <Quiz
-    id="xx"
-    name="My Quiz"
-  />
-),
-(
-  <Quiz
-    id="yy"
-    name="My Quiz 2"
-  />
-)];
 
 const QuizCatalog: FunctionComponent = () => {
+  const [quizzes, setQuizzes] = useState([{
+    id: uuidv4(),
+    name: "My Quiz"
+  }]);
+
+  function handleCreateNew(): void {
+    setQuizzes([...quizzes, {
+      id: uuidv4(),
+      name: "My Quiz"
+    }]);
+  }
 
   return (
     <div id="quizzes">
       <div id="quizzes-catalog-header">
-          <h2>{t("menu.quizzes")}</h2>
-          <button>{t("quizzes.reset")}</button>
+        <h2>{t("menu.quizzes")}</h2>
+        <button>{t("quizzes.reset")}</button>
       </div>
       <div id="quizzes-list">
-        {quizzes}
+        {quizzes.map(quiz => (
+          <Quiz
+            key={quiz.id}
+            id={quiz.id}
+            name={quiz.name}
+          />
+        ))}
       </div>
       <div id="quizzes-create">
-        <button className="floating">{"New Quiz"}</button>
+        <button className="floating" onClick={handleCreateNew}>{"New Quiz"}</button>
       </div>
     </div>
   );
-
-  // return (
-  //   <div id="quizzes">
-  //     <QuizSettings />
-  //   </div>
-  // )
 };
 
 export default QuizCatalog;
